8c92754fb39d252e5c24120016b2dcf193bae7cd
SiriyaS <siriya013@gmail.com>
2020-01-07 19:25:41 +0700

    edit nserve

--- app.js
+++ app.js
@@ -86,13 +86,14 @@ function handleMessageEvent(event) {
 
                     // The whole response has been received. Print out the result.
                     resp.on('end', () => {
-                        console.log(JSON.parse(data));
-                        console.log(JSON.parse(data).explanation);
+                        console.log(data)
+                        // console.log(JSON.parse(data));
+                        // console.log(JSON.parse(data).explanation);
                     });
 
                 }).on("error", (err) => {
                     console.log("Error: " + err.message);
-                    });
+                });
 
                 // var msg = {
                 //     type: 'text',
